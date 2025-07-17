552eaf017328e386a4d6571c236f45d2ad76b54d
Wendetton <feazegoncalves@gmail.com>
2025-07-16 22:50:51 -0300

    Update YoutubePlayer.js

--- components/YoutubePlayer.js
+++ components/YoutubePlayer.js
@@ -1,25 +1,27 @@
 import YouTube from "react-youtube";
 
 export default function YoutubePlayer({ videoId }) {
-  // Estilo para preencher a tela mantendo proporção 16:9
+  // Preenche a tela, mantendo proporção 16:9 SEM erros de sintaxe
   return (
-    <div style={{
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100vw",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "#111",
-      zIndex: 1,
-    }}>
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#111",
+        zIndex: 1,
+      }}
+    >
       <div
         style={{
           width: "100vw",
           height: "100vh",
-          maxWidth: "177.78vh",  // 16/9 = 1.7778... garante que nunca ultrapasse o limite vertical
+          maxWidth: "177.78vh", // 16/9 proporção (100vh * 16 / 9)
           maxHeight: "100vh",
         }}
       >
@@ -32,16 +34,12 @@ export default function YoutubePlayer({ videoId }) {
               autoplay: 1,
               controls: 1,
               modestbranding: 1,
-              rel: 0
-            }
+              rel: 0,
+            },
           }}
           style={{ width: "100%", height: "100%" }}
         />
       </div>
-    </div>
-  );
-}
-
     </div>
   );
 }
