6b737ae4078097b28d68f14503222b0f9610413a
Wendetton <feazegoncalves@gmail.com>
2025-07-16 23:42:00 -0300

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCYdjRgOv7A_OK2oMy5o3gGDxW-mn0ID54",
   authDomain: "webtv-ee904.firebaseapp.com",
   projectId: "webtv-ee904",
-  storageBucket: "webtv-ee904.firebasestorage.app",
+  storageBucket: "webtv-ee904.appspot.com",
   messagingSenderId: "657754370553",
   appId: "1:657754370553:web:2184cc792b71ef5e8b0d28"
 };
