a64b75c22966301733c1f1cda124eb1500d09879
Wendetton <feazegoncalves@gmail.com>
2025-07-16 23:12:52 -0300

    Create Carousel.js

---
+++ components/Carousel.js
@@ -0,0 +1,17 @@
+export default function Carousel() {
+  // Depois você faz o upload e lista das imagens no painel admin
+  return (
+    <div style={{
+      width: "100%",
+      height: "100%",
+      background: "#eee",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      color: "#888",
+      fontSize: 32
+    }}>
+      Carrossel de imagens aqui
+    </div>
+  );
+}
